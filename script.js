b39d74deb023b1ed9cdd200bf79d7748cdc18e6e
Ronak Ali <ronakalibaladi@gmail.com>
2025-03-15 11:09:57 -0400

    gid add .

---
+++ script.js
@@ -0,0 +1,17 @@
+function openTab(tabName, element) {
+    // Hide all tab content
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the "active" class from all tabs
+    var tablinks = document.getElementsByClassName("tablink");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab and add the "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    element.className += " active";
+}
